bab11b002b089bf871b86c8e0bcbbae6ab53efb7
jefflueck <lueckcfd@yahoo.com>
2025-05-16 18:12:50 -0500

    added thrust to spaceship, added script to handle key input for thrust, applied script to ship, renamed scene to sandbox, made landing pads into prefabs.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,40 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    InputAction thrust;
+
+    [SerializeField]
+    private float thrustForce = 1000f;
+
+
+    // Another convention is to call the variable "rb" for "rigidbody"
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        thrust.Enable();
     }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+
+            // Add a force to the rigidbody in the upward direction
+            rb.AddForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+
+
+        }
+
+    }
+
 }

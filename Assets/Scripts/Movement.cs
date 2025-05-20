a93aa74803741ba963b3ec1eeb309b592bfc605d
jefflueck <lueckcfd@yahoo.com>
2025-05-20 17:15:12 -0500

    added some features to unity project, added a cineamachine, added to movement script for rocket rotation.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,10 +6,14 @@ public class Movement : MonoBehaviour
 {
     [SerializeField]
     InputAction thrust;
+    [SerializeField]
+    InputAction rotation;
 
     [SerializeField]
-    private float thrustForce = 1000f;
+    private float thrustStrength = 1000f;
 
+    [SerializeField]
+    private float rotationStrength = 100f;
 
     // Another convention is to call the variable "rb" for "rigidbody"
     private Rigidbody rb;
@@ -22,19 +26,47 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
 
             // Add a force to the rigidbody in the upward direction
-            rb.AddForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
 
 
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+        else
+        {
+            // No rotation input, do nothing
         }
 
     }
 
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
 }
